e92622834574405944cb42c3d5e82051bed2ef69
Aklilegetachew <aklilegetachew@gmail.com>
2024-08-21 13:55:48 +0300

    making changes to use on one domain

--- routes/route.js
+++ routes/route.js
@@ -5,11 +5,11 @@ const auth = require("../controllers/Authentication");
 
 const router = express.Router();
 
-router.get("/register", controller.getData);
-router.post("/register", controller.addCustomer);
-router.post("/schedule", controller.addSchedule);
-router.get("/schedule", controller.showSchedule);
-router.post("/signup", auth.signup);
-router.post("/login", auth.login);
+router.get("/back/register", controller.getData);
+router.post("/back/register", controller.addCustomer);
+router.post("/back/schedule", controller.addSchedule);
+router.get("/back/schedule", controller.showSchedule);
+router.post("/back/signup", auth.signup);
+router.post("/back/login", auth.login);
 
 module.exports = router;
